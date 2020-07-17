66af9eda5aa336939e1e24d1da17a1f2e5e4ba5a
Deryck Jackson <44139628+DeryckJackson@users.noreply.github.com>
2020-07-17 10:31:55 -0700

    change bread property to numLoafs to reduce naming confusion

--- PierresBakery/Models/Bread.cs
+++ PierresBakery/Models/Bread.cs
@@ -4,23 +4,23 @@ namespace Models
 {
   public class Bread
   {
-    public int bread { get; set; }
+    public int numLoafs { get; set; }
 
     public Bread(int Bread)
     {
-      bread = Bread;
+      numLoafs = Bread;
     }
 
     public int CalcBreadPrice() 
     {
       int price = 0;
-      if (this.bread % 2 == 0)
+      if (numLoafs % 2 == 0)
       {
-        price = (this.bread / 2) * 5; 
+        price = (numLoafs / 2) * 5; 
       }
       else
       {
-        price = (this.bread / 2) * 5 + 5;
+        price = (numLoafs / 2) * 5 + 5;
       }
       return price;
     }
