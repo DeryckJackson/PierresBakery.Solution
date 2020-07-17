45beb1c604dee1965f573fa7818ff275b7c55cb2
Deryck Jackson <44139628+DeryckJackson@users.noreply.github.com>
2020-07-17 16:03:10 -0700

    add bread ascii art to console program

--- PierresBakery/Main.cs
+++ PierresBakery/Main.cs
@@ -8,8 +8,15 @@ namespace Bakery
     public static void Main()
     {
       Console.WriteLine("Welcome to Pierre's Bakery!");
+      Console.WriteLine("   _______");
+      Console.WriteLine("  /       )");
+      Console.WriteLine(" /_____   |");
+      Console.WriteLine("(  '   ) /");
+      Console.WriteLine(" |.  '| / ");
+      Console.WriteLine(" |____|/  \n");
+
       Console.WriteLine("A loaf of bread is $5 for one or Buy one get one free.");
-      Console.WriteLine("A pastry is $2 for one or 3 for $5.");
+      Console.WriteLine("A pastry is $2 for one or 3 for $5.\n");
       try
       {
         Console.WriteLine("How many loafs of bread would you like?");
