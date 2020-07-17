c32514b07771b9e49d4bf5bbd37f687675b6da53
Deryck Jackson <44139628+DeryckJackson@users.noreply.github.com>
2020-07-17 13:00:14 -0700

    add 4th failing test

--- PierresBakery.Tests/ModelsTests/PastryTests.cs
+++ PierresBakery.Tests/ModelsTests/PastryTests.cs
@@ -6,10 +6,17 @@ namespace Models.Tests
   public class PastryTests
   {
     [TestMethod]
-    public void CalcPastryPrice() 
+    public void CalcPastryPrice_1PastryEquals2() 
     {
       Pastry pastry = new Pastry(1);
       Assert.AreEqual(2, pastry.CalcPastryPrice());
+    }
+
+    [TestMethod]
+    public void CalcPastryPrice_4PastryEquals7() 
+    {
+      Pastry pastry = new Pastry(4);
+      Assert.AreEqual(7, pastry.CalcPastryPrice());
     }
   }
 }
