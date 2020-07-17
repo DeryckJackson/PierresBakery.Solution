d9e5ab3b5413a2bb47b2f13db451b4857ad057d5
Deryck Jackson <44139628+DeryckJackson@users.noreply.github.com>
2020-07-17 09:29:01 -0700

    add second failing test

--- PierresBakery.Tests/ModelsTests/BreadTests.cs
+++ PierresBakery.Tests/ModelsTests/BreadTests.cs
@@ -6,9 +6,15 @@ namespace Models.Tests
   public class BreadTests
   {
     [TestMethod]
-    public void CalcBreadPrice_OneBreadEqualsfive() 
+    public void CalcBreadPrice_OneBreadEqualsfive()
     {
       Assert.AreEqual(5, Bread.CalcBreadPrice(1));
+    }
+
+    [TestMethod]
+    public void CalcBreadPrice_ThreeBreadEqualsTen()
+    {
+      Assert.AreEqual(10, Bread.CalcBreadPrice(3));
     }
   }
 }
